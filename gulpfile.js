f52cc272d08f434f82dc195d531810c95fc82238
Jeffrey Snowdon <jeffsnowdon@gmail.com>
2016-11-05 12:59:09 -0400

    Add NPM dependencies. Add gulp build process. Add html and js entry points.

---
+++ gulpfile.js
@@ -0,0 +1,56 @@
+const browserify = require('browserify'),
+    watchify = require('watchify'),
+    gulp = require('gulp'),
+    source = require('vinyl-source-stream'),
+    gutil = require('gulp-util'),
+    stringify = require('stringify'),
+    babel = require('babelify'),
+    sourceFile = './src/main.js',
+    destFolder = './build/',
+    destFile = 'bundle.js';
+    
+
+function rebundle(b) {
+  return b
+    .transform(babel,{presets: ["es2015"]})
+    .transform(stringify, {
+      appliesTo: { includeExtensions: ['.html'] }
+    })
+    .bundle()
+    .on('error', function (err) {
+         gutil.log(err.toString());
+         this.emit("end");
+     })
+    .pipe(source(destFile))
+    .pipe(gulp.dest(destFolder));
+}
+
+gulp.task('browserify', function() {
+  var b = browserify(sourceFile,
+    {
+      cache: {},
+      packageCache: {},
+      debug:true,
+    });
+  return rebundle(b);
+});
+
+gulp.task('watch', function() {
+  var b = browserify(sourceFile,
+     {
+       cache: {},
+       packageCache: {},
+       debug:true,
+       plugin: [watchify],
+     });
+  b.on('update', function(){
+    return rebundle(b);
+  });
+  b.on('log', function(msg){
+    gutil.log(msg);
+  });
+  //watchify requires bundle to be run once before updates will work
+  return rebundle(b);
+});
+
+gulp.task('default', ['watch']);
